a516abbe1956e74721c3d0bb8434b1b9d1cf0d8a
Ryan Zezeski <rzezeski@gmail.com>
2011-02-16 22:28:11 -0500

    ability to configure default block size

--- src/luwak_file.erl
+++ src/luwak_file.erl
@@ -37,7 +37,10 @@ create(Riak, Name, Attributes) when is_binary(Name) ->
 %%                                   checksum of the most recent write request
 %%                                   as a property of the filehandle.
 create(Riak, Name, Properties, Attributes) when is_binary(Name) ->
-    BlockSize = proplists:get_value(block_size, Properties, ?BLOCK_DEFAULT),
+    DefaultBlockSize = app_helper:get_env(luwak,
+                                          default_block_size,
+                                          ?BLOCK_DEFAULT),
+    BlockSize = proplists:get_value(block_size, Properties, DefaultBlockSize),
     Order = proplists:get_value(tree_order, Properties, ?ORDER_DEFAULT),
     Checksumming = proplists:get_value(checksumming, Properties, false),
     if
